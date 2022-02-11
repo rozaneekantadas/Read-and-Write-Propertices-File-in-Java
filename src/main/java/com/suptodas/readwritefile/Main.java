17bdbdb5e58f2056c190f56b4f825b26eca8fd48
supto-das <suptodas00@gmail.com>
2022-02-12 01:03:44 +0600

    Initial Commit

---
+++ src/main/java/com/suptodas/readwritefile/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.suptodas.readwritefile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author Asus
+ */
+public class Main {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Properties properties = new Properties();
+        FileInputStream inputstream = new FileInputStream("C:\\Users\\Asus\\Documents\\NetBeansProjects\\ReadWriteFile\\src\\test\\java\\resources\\propertics\\testdata.properties");
+        properties.load(inputstream);
+        System.out.println(properties.getProperty("browser"));
+        System.out.println(properties.getProperty("url"));
+        
+        FileOutputStream outputstream = new FileOutputStream("C:\\Users\\Asus\\Documents\\NetBeansProjects\\ReadWriteFile\\src\\test\\java\\resources\\propertics\\testdata.properties");
+        properties.setProperty("testdata", "01793344213");
+        properties.store(outputstream, "This is customer phone number data");
+    }
+    
+}
